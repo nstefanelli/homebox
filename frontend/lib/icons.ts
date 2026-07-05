043ed6d232604eb49993c14704d569888f5a40cb
nstefanelli <nick.stefanelli@me.com>
2026-07-05 05:08:16 +0000

    feat: entity icon resolver + location/container icon registry

--- frontend/lib/icons.ts
+++ frontend/lib/icons.ts
@@ -14,6 +14,23 @@ import MdiWrenchOutline from "~icons/mdi/wrench-outline";
 import MdiDumbbellOutline from "~icons/mdi/dumbbell";
 import MdiSofaOutline from "~icons/mdi/sofa-outline";
 import MdiPalleteOutline from "~icons/mdi/palette-outline";
+import MdiMapMarkerOutline from "~icons/mdi/map-marker-outline";
+import MdiPackageVariant from "~icons/mdi/package-variant";
+import MdiPackageVariantClosed from "~icons/mdi/package-variant-closed";
+import MdiBasketOutline from "~icons/mdi/basket-outline";
+import MdiBookshelf from "~icons/mdi/bookshelf";
+import MdiGarage from "~icons/mdi/garage";
+import MdiWardrobeOutline from "~icons/mdi/wardrobe-outline";
+import MdiStairsDown from "~icons/mdi/stairs-down";
+import MdiHomeRoof from "~icons/mdi/home-roof";
+import MdiDoor from "~icons/mdi/door";
+import MdiArchiveOutline from "~icons/mdi/archive-outline";
+import MdiCubeOutline from "~icons/mdi/cube-outline";
+import MdiFridgeOutline from "~icons/mdi/fridge-outline";
+import MdiSafe from "~icons/mdi/safe";
+import MdiHanger from "~icons/mdi/hanger";
+import MdiTreasureChest from "~icons/mdi/treasure-chest";
+import { resolveEntityIconName, type EntityIconSource } from "./entity-icon";
 
 export const availableIcons = [
   { name: "tag-outline", component: MdiTagOutline },
@@ -32,6 +49,22 @@ export const availableIcons = [
   { name: "wrench-outline", component: MdiWrenchOutline },
   { name: "dumbbell", component: MdiDumbbellOutline },
   { name: "palette-outline", component: MdiPalleteOutline },
+  { name: "map-marker-outline", component: MdiMapMarkerOutline },
+  { name: "package-variant", component: MdiPackageVariant },
+  { name: "package-variant-closed", component: MdiPackageVariantClosed },
+  { name: "basket-outline", component: MdiBasketOutline },
+  { name: "bookshelf", component: MdiBookshelf },
+  { name: "garage", component: MdiGarage },
+  { name: "wardrobe-outline", component: MdiWardrobeOutline },
+  { name: "stairs-down", component: MdiStairsDown },
+  { name: "home-roof", component: MdiHomeRoof },
+  { name: "door", component: MdiDoor },
+  { name: "archive-outline", component: MdiArchiveOutline },
+  { name: "cube-outline", component: MdiCubeOutline },
+  { name: "fridge-outline", component: MdiFridgeOutline },
+  { name: "safe", component: MdiSafe },
+  { name: "hanger", component: MdiHanger },
+  { name: "treasure-chest", component: MdiTreasureChest },
 ] as const;
 
 export type IconName = (typeof availableIcons)[number]["name"];
@@ -46,3 +79,11 @@ export function getIconComponent(iconName: string | undefined): any {
 }
 
 export const defaultIcon = MdiTagOutline;
+
+const iconNames = availableIcons.map(i => i.name) as readonly string[];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function resolveEntityIcon(src: EntityIconSource): any {
+  const name = resolveEntityIconName(src, iconNames);
+  return availableIcons.find(i => i.name === name)!.component;
+}
